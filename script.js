605487376089af518f2f9857385fa1708dd33392
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-27 08:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John",
+  city:"Hyderabad",
+  age:18;
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
